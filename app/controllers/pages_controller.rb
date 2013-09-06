2c743cde95f964625971388bdc9487fb1c71592c
Elizabeth A Kari <eakari@live.com>
2013-09-05 17:10:44 -0700

    add favorite movie

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   def home
     @first_movie =Movie.first
     @first_count = @first_movie.votes.count
+    @my_favorite = Movie.find_by title: 'Casablanca'
   end
 
 end
